71358ce9a1a4a3a401da8afd4691ae93d453d0cf
fulopco <you@example.com>
2023-11-22 23:35:54 +0100

    add method to check if sentence ends with punctuation

--- src/main/java/com/corinna/fulop/Validator.java
+++ src/main/java/com/corinna/fulop/Validator.java
@@ -1,7 +1,10 @@
 package com.corinna.fulop;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
@@ -15,8 +18,9 @@ public class Validator {
     List<String> sentences = reader.readEachLine();
     int counter = 0;
 
-    for(int i = 0; i < sentences.size(); i++) {
-      if(hasNoDuplications(sentences.get(i)) && containsMoreThanOneWord(sentences.get(i))) {
+    for (int i = 0; i < sentences.size(); i++) {
+      if (containsMoreThanOneWord(sentences.get(i)) && endsWithPunctuation(sentences.get(i)) && hasNoDuplications(sentences.get(i))) {
+        System.out.println(sentences.get(i));
         counter++;
       }
     }
@@ -25,15 +29,23 @@ public class Validator {
     return counter;
   }
 
-  private boolean hasNoDuplications (String sentence) {
-    String[] splittedArray = sentence.split(" ");
-    Set<String> splittedSet = new HashSet<>(Arrays.asList(splittedArray));
-
-    return splittedArray.length == splittedSet.size();
-  }
-
   private boolean containsMoreThanOneWord(String sentence) {
     return sentence.split(" ").length > 1;
+  }
+
+  private boolean endsWithPunctuation(String sentence) {
+    List<Character> punctuation = Arrays.asList('!', '?', '.');
+    return punctuation.contains(sentence.charAt(sentence.length() - 1));
+  }
+
+  private boolean hasNoDuplications(String sentence) {
+    if (endsWithPunctuation(sentence)) {
+      String[] splittedArray = sentence.substring(0,sentence.length()-1).split(" ");
+      Set<String> splittedSet = new HashSet<>(Arrays.asList(splittedArray));
+
+      return splittedArray.length == splittedSet.size();
+    }
+    return false;
   }
 
 }
