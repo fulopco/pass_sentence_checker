4994bd0fe5ed89060e58a5b0e019a497b3b2bd47
fulopco <you@example.com>
2023-11-22 23:03:50 +0100

    add method to check for duplications

--- src/main/java/com/corinna/fulop/Validator.java
+++ src/main/java/com/corinna/fulop/Validator.java
@@ -1,7 +1,10 @@
 package com.corinna.fulop;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
@@ -15,7 +18,21 @@ public class Validator {
     List<String> sentences = reader.readEachLine();
     int counter = 0;
 
+    for(int i = 0; i < sentences.size(); i++) {
+      if(hasNoDuplications(sentences.get(i))) {
+        counter++;
+      }
+    }
+
+    System.out.println(counter);
     return counter;
+  }
+
+  private boolean hasNoDuplications (String sentence) {
+    String[] splittedArray = sentence.split(" ");
+    Set<String> splittedSet = new HashSet<>(Arrays.asList(splittedArray));
+
+    return splittedArray.length == splittedSet.size();
   }
 
 
